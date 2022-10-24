ad65d03e2bc29fef6101370f4d426ecf3937923f
juancruz-v <juancruzvillanueva7@gmail.com>
2022-10-23 22:41:38 -0300

    work of 23/10/22

--- MATLAB_codes/ideal_variable_frequency.m
+++ MATLAB_codes/ideal_variable_frequency.m
@@ -17,27 +17,15 @@ t_s= 1/f_s;
 % x_signal =  A_x + B_x * cos(phi_t);
 % y_signal =  A_x + B_x * cos(phi_t+phase_initial);
 
-A_x = 0; 
-A_y = 0;
-B_x = 1;
-B_y = 1;
-delay = 0;
+A_x = 7; 
+A_y = 11;
+B_x = 2;
+B_y = 37;
+delay = pi/4;
 phase_initial =0;
 %Señal continua con delay pi/2
 [x_signal, y_signal, t_line] = quadrature_signal_generator(t_s,tau,nciclos,phase_initial,f_max,A_x,A_y,B_x,B_y,delay);
 
-figure %%JC cambio de figura
-plot(x_signal,y_signal,'-')
-axis([-1 1 -1 1]), 
-title(sprintf('Frecuencia=%d Delay=pi/2',f_max))
-xlabel('Amplitud en x');
-ylabel('Amplitud en y');
-hold all
-plot(x_signal(end),y_signal(end),'og')
-hold all
-plot(x_signal(1),y_signal(1),'or')
-legend('Fase','Fase final','Fase inicial')
-ylim = [-1.5,1.5];
 
 %Comparación entre señal ideal y señal muestreada en funcion del tiempo
 
@@ -51,5 +39,31 @@ plot(t_line,y_signal,'-r')
 xlabel('tiempo')
 legend('X','Y')
 
-longitud = a_tan2(x_signal,y_signal);
+figure %%JC cambio de figura
+plot(x_signal,y_signal,'-')
+axis([-1 1 -1 1]), 
+title(sprintf('Frecuencia=%d Delay=pi/2',f_max))
+xlabel('Amplitud en x');
+ylabel('Amplitud en y');
+hold all
+plot(x_signal(end),y_signal(end),'og')
+hold all
+plot(x_signal(1),y_signal(1),'or')
+legend('Fase','Fase final','Fase inicial')
+hold all
+%ylim = [-1.5,1.5];
+displacement_without_correction = a_tan2(x_signal,y_signal);
 
+%DC Offset correction
+
+x_signal_no_DC = (max(y_signal)-min(y_signal))/2 * (x_signal-(max(x_signal)+min(x_signal))/2);
+y_signal_no_DC = (max(x_signal)-min(x_signal))/2 * (y_signal-(max(y_signal)+min(y_signal))/2);
+
+
+plot(x_signal_no_DC,y_signal_no_DC,'-r')
+hold all
+
+
+% Hacer variables los errores.
+% DC Offset correction como en la FPGA
+% DC phase correction como en la FPGA
