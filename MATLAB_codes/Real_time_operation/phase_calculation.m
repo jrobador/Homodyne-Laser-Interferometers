19d66e59255fd89214ba34143d81dcf63f305c48
Juan Cruz Villanueva <juancruzvillanueva7@gmail.com>
2023-09-20 23:01:48 -0300

    cambios al 20_9_23 alas 23_01

--- MATLAB_codes/Real_time_operation/phase_calculation.m
+++ MATLAB_codes/Real_time_operation/phase_calculation.m
@@ -1,22 +1,26 @@
 function [phase] = phase_calculation(x,y, n)
-    arguments
-        x, y, n = 0
-    end
 
     if (x > 0 && y >= 0)
        phase = atan (y/x) + 2 * pi * n;
-    end
+    else
 
-    if (x <= 0 && y > 0)
-       phase = atan (y(end)/x(end)) + pi + 2 * pi * n;
-    end
+        if (x <= 0 && y > 0)
+           phase = atan (y(end)/x(end)) + pi + 2 * pi * n;
+        else
 
-    if (x < 0 && y <= 0)
-       phase = atan (y/x) + pi + 2 * pi * n;
-    end
+            if (x < 0 && y <= 0)
+               phase = atan (y/x) + pi + 2 * pi * n;
+            else
 
-    if (x(end) >= 0 && y < 0)
-       phase = atan (y/x) + 2*pi + 2 * pi * n;
-    end   
+                if (x(end) >= 0 && y < 0)
+                   phase = atan (y/x) + 2*pi + 2 * pi * n;
+                else
+                    phase = -1;
+                end   
+            end
+        end
+    end
+    
+    
 end
 
